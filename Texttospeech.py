a0f39c9b8bdb12a37b66f14149bbe50cc77e7fdb
idyllic771 <idyllicmary771@gmail.com>
2023-04-03 20:56:02 +0000

    Text to speech submission

--- Texttospeech.py
+++ Texttospeech.py
@@ -1,56 +1,51 @@
-import tkinter as tk
 import pyttsx3
+import PySimpleGUI as sg
 
-window = tk.Tk()
-window.title("Text to Speech Application")
-window.configure(background="pink")
-window.geometry("400x200")
+layout = [
+    [sg.Text('Please enter text to:',background_color='pink',text_color='black')],
+    [sg.Input()],
+    [sg.Radio('Female voice', 'VOICE', key='female',default=True,background_color='pink',text_color='black'),
+     sg.Radio('Male voice', 'VOICE', key='male',background_color='pink',text_color='black')],
+    [sg.Button('Speak',button_color=('white', 'green'))]
+]
+
+window = sg.Window('Text to Speech',layout,background_color='pink')
+
 engine = pyttsx3.init()
-gender = tk.StringVar()
 
-
-def speech():
-    voice = gender.get()
-    sentence = entry.get()
-    # print(voice)
-    if voice == "female":
-        female_voice = engine.getProperty('voices')[1]
-        engine.setProperty('voice', female_voice.id)
-        engine.say(sentence)
-        volume = engine.getProperty('volume')   
-        engine.setProperty('volume',1.0)
-        #print (volume) 
+def speak(text, voice):
+    if voice == 'male':
+        engine.setProperty('voice', 'english+m7')
+        engine.setProperty('rate', 100)  
+        engine.setProperty('volume', 1.0)
+        voices = engine.getProperty('voices')
+        engine.setProperty('voice', voices[0].id) 
+        engine.say(text)
         engine.runAndWait()
-    elif voice == "male":
-        male_voice = engine.getProperty('voices')[0]
-        engine.setProperty('voice', male_voice.id)
-        engine.say(sentence)
-        volume = engine.getProperty('volume')
-        engine.setProperty('volume',1.0)
-        #print (volume) 
+    elif voice == 'female':
+        engine.setProperty('voice', 'english+f4')
+        engine.setProperty('rate', 100)  
+        engine.setProperty('volume', 1.0)
+        engine.say(text)
         engine.runAndWait()
 
     
-label1 = tk.Label(text="select voice types", bg="pink")
-entry = tk.Entry(bg="white", width=40)
 
-button = tk.Button(
-    text="Speech", command=speech, bg="green"
+    
 
-)
-
-R1 = tk.Radiobutton(text="Male", value="male", variable=gender, fg="black",
-                    bg="pink",
-                    )
-R2 = tk.Radiobutton(text="Female", value="female", variable=gender, fg="black",
-                    bg="pink",
-                    )
-
-entry.grid(row=1, column=0, padx=5, pady=5)
-button.grid(row=1, column=1, padx=5, pady=5)
-label1.grid(row=2, column=0)
-R1.grid(row=2, column=1)
-R2.grid(row=2, column=2)
-gender.set(None)
-
-window.mainloop()
+while True:
+    event,value = window.read()
+    if event == sg.WIN_CLOSED:
+     break
+    if event == 'Speak':
+       text = value[0]
+       if value['female'] == True:
+          voices = engine.getProperty('voices')
+          engine.setProperty('voice', voices[1].id)
+          engine.say(text)
+          engine.runAndWait()
+       elif value['male'] == True:
+          voices = engine.getProperty('voices')
+          engine.setProperty('voice', voices[0].id)
+          engine.say(text)
+          engine.runAndWait()
